7c2f0b3e4d2b407702dba16c3acf3d0ee3f14294
Ridvan Korurer <79053825+MRidvanKorurer@users.noreply.github.com>
2024-05-07 17:47:20 +0300

    server klasörü kuruldu server ayağa kalktı

---
+++ server/src/app.ts
@@ -0,0 +1,29 @@
+import express, {Express} from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import conn from "./db/connectDb";
+
+
+dotenv.config();
+
+
+const app: Express = express();
+const port: string | number = process.env.PORT || 3000;
+
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+
+// routing
+
+
+
+// error
+
+
+app.listen(port, () => {
+    conn();
+    console.log(`Server is running on port: ${port}`);
+})
